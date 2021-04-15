d359d146259f9abcf7836c7c88449328d799b49b
Pablo Bernal Alarcón <bononov6@yahoo.com>
2021-04-15 12:09:15 -0500

    Cursed Kenny
    Added new functionality for buttons
    Buttons can control a character
    Added Kenny from South Park
    Began working with event triggers

---
+++ Cerros AR/Assets/Pablo Sr/3D/Nintendo 64 - South Park - Kenny/Kenny/README.txt
@@ -0,0 +1,6 @@
+Re-Ripped by Hell Inspector under the submitter name "Hawkodile"
+https://www.vg-resource.com/user-39500.html
+
+Credit isn't really needed.
+
+Thank you and Have a great day! Have fun using this model.
--- Cerros AR/Assets/Pablo Sr/Scripts/ShitpostCube.cs
+++ Cerros AR/Assets/Pablo Sr/Scripts/ShitpostCube.cs
@@ -5,26 +5,48 @@ using UnityEngine.UI;
 
 public class ShitpostCube : MonoBehaviour
 {
-
     public float RotationSpeed;
+    public Transform gameCam;
 
     public Slider SpeedSlider;
     Vector3 RotateBy;
+
+    Vector3 Xtransition;
     // Start is called before the first frame update
     void Start()
     {
         RotateBy = new Vector3(0,RotationSpeed*Time.deltaTime,0);
+        Xtransition = new Vector3(0, 0, -1);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.LookAt(gameCam);
       RotateBy = new Vector3(0, RotationSpeed * Time.deltaTime, 0);
         transform.Rotate(RotateBy);
+        Debug.Log("DT: "+Time.deltaTime);
+
+        if (moving==true)
+        {
+            transform.position += (Xtransition * Time.deltaTime);
+        }
     }
 
     public void SetSpeed()
     {
         RotationSpeed = SpeedSlider.value;
     }
+
+    bool moving = false;
+    public void MoveKenny()
+    {
+        moving = true;
+    }
+    public void StopKenny()
+    {
+        moving = false;
+    }
+
 }
